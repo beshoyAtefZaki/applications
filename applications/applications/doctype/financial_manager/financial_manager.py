b6fd25ede98dd85282d5caeaa0dd50ee65e3152f
beshoy <beshoyatef31@gmail.com>
2019-12-05 14:15:42 +0300

    update Gm

--- applications/applications/doctype/financial_manager/financial_manager.py
+++ applications/applications/doctype/financial_manager/financial_manager.py
@@ -11,13 +11,13 @@ from applications.utilites import create_gm_app ,update_status
 class Financialmanager(Document):
 	def on_submit(self):
 		if self.manager_approved == "Approved":
-			if self.g_m_approved == '1':
-				create_manager_app("G M Applications",self.employee_applications ,
+			if self.g_m_approved == 1:
+				create_gm_app("G M Applications",self.employee_applications ,
 									self.employee , self.employee_name , "Approved" ,
 									self.refrence ,self.refrence_type ,
 									self.applications , self.attachment)
 
-			if self.g_m_approved == '0':
+			if self.g_m_approved == 0:
 				status ="Approved"
 				update_status(status ,self.employee_applications)
 		if self.manager_approved == "Rejected":
