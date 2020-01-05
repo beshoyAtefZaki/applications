1ce1f5362d1ef74a4234d6ed329052d10c3d3166
beshoy <beshoyatef31@gmail.com>
2020-01-05 16:50:59 +0300

    UPdate

--- applications/applications/doctype/employee_applications/employee_applications.js
+++ applications/applications/doctype/employee_applications/employee_applications.js
@@ -2,37 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee applications', {
-	onload: function(frm) {
-    var employee = frappe.session.user ;
-    frappe.call({
-    method:'applications.applications.doctype.employee_applications.employee_applications.get_employee',
-    args: {
-      'usr'  : employee
-    } ,
-    callback: function(r){
+	application_type:function(frm){
+		var type = frm.doc.application_type
+		if (type == "Accounting Application"){
 
-			frm.set_value("employee" ,r.message.name)
-			frm.set_value("employee_name" ,r.message.employee_name)
+				frm.set_value("ap_status" ,"Processing To Accounting")
+		}		else {
+				frm.set_value("ap_status" , "Processing To HR")
+		}
 
-    }
-
-
-})
-},
+	},
 	employee :  function(frm) {
     var employee = frm.doc.employee ;
-		
     frappe.call({
     method:'applications.applications.doctype.employee_applications.employee_applications.get_employee',
     args: {
       'usr'  : employee
     } ,
     callback: function(r){
-			if (r.messgae){
-				frm.set_value("employee" ,r.message.name)
-				frm.set_value("employee_name" ,r.message.employee_name)
+				frm.set_value("employee_name" ,r.message[0].employee_name)
 			}
-    }
+
 
 
 })
--- applications/applications/doctype/employee_applications/employee_applications.py
+++ applications/applications/doctype/employee_applications/employee_applications.py
@@ -9,14 +9,15 @@ from applications.utilites import create_manager_app , send_mail
 
 
 class Employeeapplications(Document):
-		def on_submit(self):
-
-				
-				send_mail(self.employee_name,str(self.name))
-				status = "Processing"
-				self.ap_status = status
-				create_manager_app("Manager applications", self.name , self.employee , self.employee_name ,status  ,
-								self.applications , self.attachment)
+		pass
+		# def on_submit(self):
+		#
+		#
+		# 		send_mail(self.employee_name,str(self.name))
+		# 		status = "Processing"
+		# 		self.ap_status = status
+		# 		create_manager_app("Manager applications", self.name , self.employee , self.employee_name ,status  ,
+		# 						self.applications , self.attachment)
 
 
 
@@ -25,8 +26,7 @@ class Employeeapplications(Document):
 @frappe.whitelist()
 def get_employee(usr):
 	res = frappe.db.sql('''
-		SELECT name, employee_name FROM `tabEmployee` WHERE user_id = '%s'
+		SELECT employee_name FROM `tabEmployee` WHERE employee = '%s'
 		'''%usr, as_dict=1)
-	for i in res :
-		return (i)
-	return None
+
+	return res
